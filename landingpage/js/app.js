eebfc73aa5172571a8e9ca5b901ad598dbc2e18d
Phyu Sin Thant <phyusinthantnext24@gmail.com>
2022-08-06 15:14:25 +0630

    BCJ templates

---
+++ landingpage/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+// console.log('hay');
+	// START SHOWCASE SLIDER SECTION
+
+	$('.carousel').carousel({
+		interval:6000,
+		pause:'hover'
+	});
+
+	// END SHOWCASE SLIDER SECTION
+});
